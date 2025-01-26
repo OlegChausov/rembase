9afe2c18e473d165ba6d72ddf48db58a62f511e7
Oleg Chausov <olegshak1985@gmail.com>
2025-01-26 13:04:50 +0300

    added active check box

--- fixorder/todo.py
+++ fixorder/todo.py
@@ -3,7 +3,7 @@ Add action buttons on the 'fixorder/orderlist.html'
 Add action buttons on the 'show_end_edit_order.html'
 Add action buttons on the 'new_order.html'
 Add auto-completeng to time_away
-Add field remain to pay to model
+Add field remain to pay to model of template solution
 Add field conclusion to model
 Make print button invisible for print
 
